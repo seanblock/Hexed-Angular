99b84263991d2150e2b902f26afccdf8ba1f0cd2
kylezehaoqin <-l>
2022-02-24 01:07:30 -0500

    wtf

--- hexed/src/app/app.component.ts
+++ hexed/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +9,27 @@ export class AppComponent {
 
   @Input() colors = ['red', 'green', 'blue'];
   @Input() gameStatus = true
-  secondsToTimer: number = 0;
-  end = false;
-  randomColor: Array<number> = [];
-  myColor: Array<number> = [50, 50, 50];
-  myRgb = "";
-  timeLeft = 0;
-  title = 'hexed';
+  @Input() settingStatus = false
+  secondsToTimer: number;
+  end: boolean;
+  randomColor: Array<number>;
+  myColor: Array<number>;
+  myRgb: string;
+  timeLeft: number;
+  title: string;
 
   fever = new Audio('../assets/fever.mp3');
   win = new Audio('../assets/win.mp3');
+
+  constructor() {
+    this.secondsToTimer = 0;
+    this.end = false;
+    this.randomColor = [];
+    this.myColor = [50, 50, 50];
+    this.myRgb = "";
+    this.timeLeft = 0;
+    this.title = 'hexed';
+  }
   
   sendToTimer(data: any) {
     this.secondsToTimer = parseInt(data);
@@ -26,13 +37,18 @@ export class AppComponent {
   }
 
   newGame() {
-    this.win.currentTime = 0
-    this.win.pause()
-  } 
+    
+    this.startGame()
+  }
+
+  restart() {
+    this.gameStatus = true
+  }
 
   startGame() {
     console.log("Game Started")
-    this.newGame()
+    this.win.currentTime = 0
+    this.win.pause()
     this.fever.play()
     this.gameStatus = false
   }
@@ -42,7 +58,7 @@ export class AppComponent {
     this.fever.currentTime = 0
     this.fever.pause()
     this.win.play()
-    //this.gameStatus = true
+    // this.gameStatus = true
     this.end = true;
   }
 
@@ -68,3 +84,4 @@ export class AppComponent {
     }
   }
 }
+
